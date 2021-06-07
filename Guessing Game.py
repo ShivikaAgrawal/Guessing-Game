34ca8fafb48115e76000f7daa1b4587d9f501b69
ShivikaAgrawal <74872250+ShivikaAgrawal@users.noreply.github.com>
2021-06-07 17:06:28 +0530

    Update Guessing Game.py
    Fun Time!! Can You Guess?

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
-
 
 # Create a new window and set the dimensions
 window = tk.Tk()
@@ -16,16 +14,9 @@ window.resizable(width=False,height=False)
 # Window's Title
 window.title('Number Guessing Game')
 
-
-# In[6]:
-
-
 #Create random variable
 TARGET = random.randint(0, 1000)
 RETRIES = 0
-
-
-# In[7]:
 
 
 def update_result(text):
@@ -38,10 +29,6 @@ def new_game():
     TARGET = random.randint(0, 1000)
     RETRIES = 0
     update_result(text="Guess a number between\n 1 and 1000")
-
-
-# In[8]:
-
 
 def play_game():
     global RETRIES
@@ -66,35 +53,23 @@ def play_game():
     update_result(result)
 
 
-# In[9]:
 
-
-# Heading of our game
 title = tk.Label(window,text="Guessing Game",font=("Arial",24),fg="#fffcbd",bg="#065569")
  
-# Result and hints of our game
 result = tk.Label(window, text="Click on Play to start a new game", font=("Arial", 12, "normal", "italic"),fg = "White", bg="#065569", justify=tk.LEFT)
  
-# Play Button
+
 play_button = tk.Button(window, text="Play Game", font=("Arial", 14, "bold"), fg = "Black", bg="#29c70a", command=new_game)
  
-# Guess Button
+
 guess_button = tk.Button(window,text="Guess",font=("Arial",13), state='disabled', fg="#13d675",bg="Black", command=play_game)
  
 # Exit Button
 exit_button = tk.Button(window,text="Exit Game",font=("Arial",14), fg="White", bg="#b82741", command=exit)
  
-
-
-# In[10]:
-
-
 # Entry Fields
 guessed_number = tk.StringVar()
 number_form = tk.Entry(window,font=("Arial",11),textvariable=guessed_number)
-
-
-# In[11]:
 
 
 # Place the labels
@@ -110,16 +85,8 @@ play_button.place(x=170, y=320)
 # Place the entry field
 number_form.place(x=180, y=150)
 
-
-# In[12]:
-
-
 # Start the window
 window.mainloop()
-
-
-# In[ ]:
-
 
 
 
